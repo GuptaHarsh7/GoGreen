810d8b75cc1bac454d3ad69f0518d24a6e1fb0b5
Suryaprakash Agarwal <42642288+Surya1231@users.noreply.github.com>
2020-01-14 21:36:16 +0530

    admin updated
    G

--- admin/adminheader.php
+++ admin/adminheader.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <?php
-  include("config.php");
+  include("../config.php");
 ?>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
     <link rel="stylesheet" href="../lib/Fontawesome/css/all.min.css">
     <link rel="stylesheet" href="../lib/Bootsrap/css/bootstrap.min.css">
