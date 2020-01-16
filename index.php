e331e5e955587232a2e3035a63852fa03fc8454f
anmolmittal26 <2017umt1501@mnit.ac.in>
2020-01-16 19:29:25 +0530

    dddd

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include("header.php"); ?>
+<title>GoGreen</title>
+<link rel="stylesheet" href="css/master.css">
+</head>
+<body>
+<?php include('topbar.php');?>
+
+<div id="index">
+
+</div>
+
+<?php include('footer.php')?>
