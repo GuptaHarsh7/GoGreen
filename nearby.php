1064b8a164bfc7a9d4da25c9b8caa3901823c3d6
anmolmittal26 <2017umt1501@mnit.ac.in>
2020-01-18 08:23:14 +0530

    nearby

--- nearby.php
+++ nearby.php
@@ -4,11 +4,29 @@
 <link rel="stylesheet" href="css/footer2.css">
 <link  rel="stylesheet"  href="http://cdn.leafletjs.com/leaflet-0.7.5/leaflet.css"  />
 <script  src="http://cdn.leafletjs.com/leaflet-0.7.5/leaflet.js"></script>
+<style media="screen">
+
+  .nearby{
+    width: 80%;
+    height: 500px;
+    overflow: auto;
+    margin: 50px auto;
+    background-image: url('images/nearby.png');
+    background-size: contain;
+    background-position: center center;
+    outline: 5px solid black;
+    outline-offset: 5px;
+    border-radius: 4px;
+  }
+  #topbar{
+    position: relative !important;
+  }
+</style>
 </head>
 <body>
 <?php include('topbar.php');?>
 
-<div style="">
-
+<div class="nearby">
 </div>
+
 <?php include('footer2.php'); ?>
