254697045299ccbfdf184691b14d566cbc45fbaa
anmolmittal26 <2017umt1501@mnit.ac.in>
2020-01-18 06:22:55 +0530

    nearby page added

--- topbar.php
+++ topbar.php
@@ -12,7 +12,7 @@
         <li><a href="events.php" class="btn btn-sm btn-outline-success">Event</a></li>
         <li><a href="#" class="btn btn-sm btn-outline-success">Challenges</a></li>
         <li><a href="issues.php" class="btn btn-sm btn-outline-success">Issues</a></li>
-        <li><a href="#" class="btn btn-sm btn-outline-success">Nearby</a></li>
+        <li><a href="nearby.php" class="btn btn-sm btn-outline-success">Nearby</a></li>
         <li><a href="#" class="btn btn-sm btn-outline-success" data-toggle="modal" data-target="#exampleModalCenter">More</a></li>
         <?php  if($_SESSION["user"]!=null) {?>
         <li><a href="" class="btn btn-sm btn-outline-success"  data-toggle="modal" data-target="#exampleModal2">Profile</a></li>
